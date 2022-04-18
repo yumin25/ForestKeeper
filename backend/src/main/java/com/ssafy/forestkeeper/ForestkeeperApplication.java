6c2c7d4a029daf8532f2d1bea4d91777c931351d
gyh8781 <gyh8781@gmail.com>
2022-04-18 13:46:15 +0900

    init backend

---
+++ backend/src/main/java/com/ssafy/forestkeeper/ForestkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.forestkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestkeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestkeeperApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ssafy/forestkeeper/ForestkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.forestkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestkeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
