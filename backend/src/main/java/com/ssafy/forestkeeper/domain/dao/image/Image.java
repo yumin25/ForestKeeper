a12214d727e27ecd80b2a0029ddaca0eb613941d
parkkibeom <kibum414@naver.com>
2022-04-20 03:56:52 +0900

    init: set entities

---
+++ backend/src/main/java/com/ssafy/forestkeeper/domain/dao/image/Image.java
@@ -0,0 +1,39 @@
+package com.ssafy.forestkeeper.domain.dao.image;
+
+import com.ssafy.forestkeeper.domain.dao.BaseEntity;
+import com.ssafy.forestkeeper.domain.dao.plogging.Plogging;
+import com.ssafy.forestkeeper.domain.dao.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image extends BaseEntity {
+
+    @Column(name = "image_original_file_name")
+    private String originalFileName;
+
+    @Column(name = "image_saved_file_name")
+    private String savedFileName;
+
+    @Column(name = "image_is_resized")
+    private boolean resize;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plogging_id")
+    private Plogging plogging;
+
+    public void changeResize() {
+        this.resize = true;
+    }
+
+}
---
+++ backend/src/main/java/com/ssafy/forestkeeper/domain/dao/user/User.java
@@ -0,0 +1,47 @@
+package com.ssafy.forestkeeper.domain.dao.user;
+
+import com.ssafy.forestkeeper.domain.dao.BaseEntity;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_nickname")
+    private String nickname;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_password")
+    private String password;
+
+    @Column(name = "user_is_deleted")
+    private boolean delete;
+
+    // 닉네임 변경
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    // 비밀번호 변경
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    // 회원 탈퇴
+    public void changeDelete() {
+        this.delete = true;
+    }
+
+}
