6c458d71e0c6e07ccba0c4cbc720bb3dbf1d0adc
parkkibeom <kibum414@naver.com>
2022-04-20 03:58:43 +0900

    init: common code

---
+++ backend/src/main/java/com/ssafy/forestkeeper/util/converter/UserConverter.java
@@ -0,0 +1,17 @@
+package com.ssafy.forestkeeper.util.converter;
+
+import com.ssafy.forestkeeper.domain.enums.UserCode;
+
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class UserConverter extends AbstractEnumAttributeConverter<UserCode> {
+
+    public static final String ENUM_NAME = "회원";
+
+    public UserConverter() {
+        super(UserCode.class, false, ENUM_NAME);
+    }
+
+}
+
