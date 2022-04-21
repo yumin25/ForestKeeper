e6804bd3689577c8bc37606774effc02c44d8228
kibum414 <kibum414@naver.com>
2022-04-21 21:17:37 +0900

    rename: move community modify patch dto

--- backend/src/main/java/com/ssafy/forestkeeper/application/dto/request/community/CommunityModifyPatchDTO.java
+++ backend/src/main/java/com/ssafy/forestkeeper/application/dto/request/community/CommunityModifyPatchDTO.java
@@ -1,4 +1,4 @@
-package com.ssafy.forestkeeper.application.dto.request.comment;
+package com.ssafy.forestkeeper.application.dto.request.community;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
