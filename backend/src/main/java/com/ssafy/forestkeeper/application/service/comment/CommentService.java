7a3581a2de3c03867ce2e8930ff349625dc8a8db
kibum414 <kibum414@naver.com>
2022-04-21 14:16:59 +0900

    feat: register comment

---
+++ backend/src/main/java/com/ssafy/forestkeeper/application/service/comment/CommentService.java
@@ -0,0 +1,16 @@
+package com.ssafy.forestkeeper.application.service.comment;
+
+import com.ssafy.forestkeeper.application.dto.request.comment.CommentRegisterPostDTO;
+
+public interface CommentService {
+
+    // 댓글 작성
+    void registerComment(CommentRegisterPostDTO commentRegisterPostDTO);
+
+    // 댓글 리스트 조회
+
+    // 댓글 수정
+
+    // 댓글 삭제
+
+}
