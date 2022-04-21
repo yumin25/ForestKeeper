163c53e5cf0d2c1249afb72b18d022c063e58521
GongYunHwan <rhddbsghks@ajou.ac.kr>
2022-04-21 14:55:08 +0900

    feat: 등산로 API

---
+++ backend/src/main/java/com/ssafy/forestkeeper/api/controller/MountainController.java
@@ -0,0 +1,57 @@
+package com.ssafy.forestkeeper.api.controller;
+
+import com.ssafy.forestkeeper.application.dto.request.user.UserSignUpDTO;
+import com.ssafy.forestkeeper.application.dto.response.BaseResponseDTO;
+import com.ssafy.forestkeeper.application.dto.response.mountain.MountainTrailResponseDTO;
+import com.ssafy.forestkeeper.application.service.user.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "User API", tags = {"User"})
+@CrossOrigin("*")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/mountain")
+public class MountainController {
+
+    private final UserService userService;
+
+    @ApiOperation(value = "등산로")
+    @GetMapping
+    public ResponseEntity<?> getMountainTrail(String mountainCode) {
+
+        JSONObject trail;
+        System.out.println(System.getProperty("user.dir"));
+
+        try {
+            ClassPathResource cpr = new ClassPathResource("trail/" + mountainCode + ".json");
+
+            byte[] bdata = FileCopyUtils.copyToByteArray(cpr.getInputStream());
+            String jsonTxt = new String(bdata, StandardCharsets.UTF_8);
+
+            trail = (JSONObject) new JSONParser().parse(jsonTxt);
+
+        } catch (Exception e) {
+            System.err.println(e);
+            return ResponseEntity.status(404).body(BaseResponseDTO.of("데이터가 존재하지 않습니다.", 404));
+        }
+
+        return ResponseEntity.status(200).body(MountainTrailResponseDTO.of("등산로 불러오기에 성공했습니다.", 200, trail));
+    }
+}
---
+++ backend/src/main/java/com/ssafy/forestkeeper/application/dto/response/mountain/MountainTrailResponseDTO.java
@@ -0,0 +1,35 @@
+package com.ssafy.forestkeeper.application.dto.response.mountain;
+
+import com.ssafy.forestkeeper.application.dto.response.BaseResponseDTO;
+import com.ssafy.forestkeeper.application.dto.response.user.UserLoginResponseDTO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.json.simple.JSONObject;
+
+@ApiModel("MountainTrailResponseDTO")
+@Setter
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class MountainTrailResponseDTO extends BaseResponseDTO {
+
+    @ApiModelProperty(name = "등산로")
+    private JSONObject trail;
+
+    public static MountainTrailResponseDTO of(String message, Integer statusCode,
+        JSONObject trail) {
+
+        MountainTrailResponseDTO mountainTrailResponseDTO = new MountainTrailResponseDTO();
+        mountainTrailResponseDTO.setTrail(trail);
+        mountainTrailResponseDTO.setMessage(message);
+        mountainTrailResponseDTO.setStatusCode(statusCode);
+
+        return mountainTrailResponseDTO;
+
+    }
+}
