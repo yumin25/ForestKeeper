8ce3a77c2df51f159ff956691a4054cfa25f4a33
김유민 <elin0225@naver.com>
2022-04-22 10:09:16 +0900

    refactor: 로그인 api

--- backend/src/main/java/com/ssafy/forestkeeper/application/service/user/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/forestkeeper/application/service/user/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ssafy.forestkeeper.application.service.user;
 
+import com.ssafy.forestkeeper.application.dto.request.user.UserLoginDTO;
 import com.ssafy.forestkeeper.application.dto.request.user.UserSignUpDTO;
 import com.ssafy.forestkeeper.domain.dao.user.User;
 import com.ssafy.forestkeeper.domain.repository.user.UserRepository;
+import com.ssafy.forestkeeper.security.util.JwtAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,12 +17,16 @@ import java.util.regex.Pattern;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    @Autowired
     private final UserRepository userRepository;
+    private final JwtAuthenticationProvider jwtProvider;
 
     // 회원가입
     @Override
     public Integer signUp(UserSignUpDTO userDTO) {
+        if(!isValidName(userDTO.getName())) return 4091;
+        else if(!isValidNickname(userDTO.getNickname())) return 4092;
+        else if(!isValidPassword(userDTO.getPassword())) return 4093;
+
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         userDTO.setPassword(encoder.encode(userDTO.getPassword()));
         userRepository.save(User.builder()
@@ -31,6 +37,18 @@ public class UserServiceImpl implements UserService {
                 .roles("ROLE_USER")
                 .build());
         return 201;
+    }
+
+    //로그인
+    @Override
+    public String login(UserLoginDTO userLoginDTO) {
+        User user = userRepository.findByEmailEquals(userLoginDTO.getEmail());
+        if (user == null) return "401";
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if(!encoder.matches(userLoginDTO.getPassword(), user.getPassword())) return "401";
+        String token = jwtProvider.createToken(userLoginDTO.getEmail());
+        System.out.println(token);
+        return token;
     }
 
     @Override
