b52c781540f192f77747a870ab48681039c24c0c
김유민 <elin0225@naver.com>
2022-04-25 15:41:41 +0900

    feat: webview

--- android/app/src/main/java/com/ssafy/forestkeeper/MainActivity.kt
+++ android/app/src/main/java/com/ssafy/forestkeeper/MainActivity.kt
@@ -2,10 +2,25 @@ package com.ssafy.forestkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var webView: WebView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.google.com/")
+    }
+
+    override fun onBackPressed() {
+        var webView: WebView = findViewById(R.id.webview)
+        if (webView.canGoBack()){
+            webView.goBack()
+        }
+        else{
+            finish()
+        }
     }
 }
