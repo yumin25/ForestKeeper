a2f780d66354b05de17a607c6bab8146500d88a1
sangjin <toramx5@gmail.com>
2022-04-26 20:02:54 +0900

    feat: login

--- frontend/src/router/accounts/Login.js
+++ frontend/src/router/accounts/Login.js
@@ -1,8 +1,11 @@
 import logo from "../../res/img/logo.png";
 import "./Login.css";
 import { useState } from "react";
+import { connect } from "react-redux";
+import { save } from "../../store/user";
+import Send from "../../config/Send";
 
-function Login() {
+function Login({ saveUser, userSlice }) {
   const [email, setEmail] = useState("");
   const onEmailHandler = (e) => {
     setEmail(e.target.value);
@@ -11,6 +14,41 @@ function Login() {
   const [password, setPassword] = useState("");
   const onPasswordHandler = (e) => {
     setPassword(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    Send.post(`/user/login`, JSON.stringify(data))
+      .then((res) => {
+        if (res.status === 202) {
+          alert("이메일 및 비밀번호를 확인해주세요.");
+          return;
+        }
+        window.localStorage.setItem("idToken", JSON.stringify(res.data.accessToken));
+        console.log(res.data);
+        // Send.get(`userinfo/seq/${res.data.userSeq}`).then((response) => {
+        //   saveUser(response.data);
+        // });
+        // history({
+        //   pathname: "/",
+        //   props: {
+        //     userId: data.userId,
+        //     userSeq: res.data.userSeq,
+        //     userCreatableCount: res.data.userCreatableCount,
+        //   },
+        // });
+      })
+      .catch((e) => console.log(e));
+  };
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      onSubmit(e);
+    }
   };
 
   return (
@@ -23,7 +61,7 @@ function Login() {
         </div>
         <div style={{ marginBottom: "2rem" }}>
           <p style={{ color: "#69696C", marginBottom: "0.5rem" }}>비밀번호</p>
-          <input type="password" className="input" placeholder="********" onChange={onPasswordHandler} />
+          <input type="password" className="input" placeholder="********" onChange={onPasswordHandler} onKeyPress={handleEnter} />
         </div>
         <button
           style={{
@@ -35,6 +73,7 @@ function Login() {
             color: "white",
             marginBottom: "0.5rem",
           }}
+          onClick={onSubmit}
         >
           로그인
         </button>
@@ -56,4 +95,12 @@ function Login() {
   );
 }
 
-export default Login;
+function mapStateToProps(state) {
+  return { userSlice: state.user };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { saveUser: (user) => dispatch(save(user)) };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
