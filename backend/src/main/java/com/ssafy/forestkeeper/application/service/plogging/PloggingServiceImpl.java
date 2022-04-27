0d8a6b3973b2ac8f03e0bc4d4ecad13f3bfb0c77
염형덕 <gud_ejr@naver.com>
2022-04-27 16:45:23 +0900

    feat: 유저의 플로깅 목록 조회 API

--- backend/src/main/java/com/ssafy/forestkeeper/application/service/plogging/PloggingServiceImpl.java
+++ backend/src/main/java/com/ssafy/forestkeeper/application/service/plogging/PloggingServiceImpl.java
@@ -4,13 +4,17 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.forestkeeper.application.dto.request.plogging.ExpRegisterDTO;
 import com.ssafy.forestkeeper.application.dto.request.plogging.PloggingRegisterDTO;
-import com.ssafy.forestkeeper.application.dto.response.comment.CommentGetListResponseDTO;
+import com.ssafy.forestkeeper.application.dto.response.community.CommunityGetListResponseDTO;
+import com.ssafy.forestkeeper.application.dto.response.community.CommunityGetListWrapperResponseDTO;
 import com.ssafy.forestkeeper.application.dto.response.plogging.PloggingDetailResponseDTO;
+import com.ssafy.forestkeeper.application.dto.response.plogging.PloggingListResponseDTO;
+import com.ssafy.forestkeeper.application.dto.response.plogging.PloggingListWrapperResponseDTO;
 import com.ssafy.forestkeeper.application.dto.response.plogging.TrashCanDTO;
 import com.ssafy.forestkeeper.application.dto.response.plogging.TrashCanListWrapperResponseDTO;
 import com.ssafy.forestkeeper.domain.dao.plogging.Plogging;
@@ -96,5 +100,33 @@ public class PloggingServiceImpl implements PloggingService{
 				.list(trashCanDTOList)
 				.build();
 	}
+
+	@Override
+	public PloggingListWrapperResponseDTO getPloggingList(int page) {
+        List<Plogging> ploggingList = ploggingRepository.findByUserId(userRepository.findByEmailAndDelete(SecurityContextHolder.getContext().getAuthentication().getName(),false).get().getId(),PageRequest.of(page - 1, 10))
+                .orElseThrow(() -> new IllegalArgumentException("글을 찾을 수 없습니다."));
+
+    	List<PloggingListResponseDTO> ploggingListResponseDTOList = new ArrayList<>();
+
+    	ploggingList.forEach(plogging ->
+    		ploggingListResponseDTOList.add(
+                        PloggingListResponseDTO.builder()
+                        		.date(plogging.getStartTime().toLocalDate().toString())
+                        		.ploggingId(plogging.getId())
+                        		.distance(plogging.getDistance())
+                        		.time(plogging.getDurationTime())
+                        		.exp(plogging.getExp())
+                        		.mountainName(plogging.getMountain().getName())
+                                .build()
+                )
+        );
+
+        return PloggingListWrapperResponseDTO.builder()
+                .list(ploggingListResponseDTOList)
+                .build();
+    	
+	}
+	
+
 
 }
