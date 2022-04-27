9a49f7dd415a7e860c4719fb95254d2d76f35774
염형덕 <gud_ejr@naver.com>
2022-04-28 03:22:04 +0900

    feat: 방문한 산 목록 API

--- backend/src/main/java/com/ssafy/forestkeeper/api/controller/UserInfoController.java
+++ backend/src/main/java/com/ssafy/forestkeeper/api/controller/UserInfoController.java
@@ -1,5 +1,8 @@
 package com.ssafy.forestkeeper.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.forestkeeper.application.dto.response.BaseResponseDTO;
+import com.ssafy.forestkeeper.application.dto.response.mountain.MountainNameListResponseDTO;
 import com.ssafy.forestkeeper.application.dto.response.plogging.PloggingListWrapperResponseDTO;
 import com.ssafy.forestkeeper.application.service.plogging.PloggingService;
+import com.ssafy.forestkeeper.application.service.userinfo.UserInfoService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -25,14 +30,16 @@ public class UserInfoController {
 	
 	private final PloggingService ploggingService;
 
-    @ApiOperation(value = "플로깅 목록 조회")
-    @GetMapping
-    public ResponseEntity<? extends BaseResponseDTO> getList(@ApiParam(value = "페이지 번호") @RequestParam(defaultValue = "1") int page) {
+	private final UserInfoService userInfoService;
+
+	@ApiOperation(value = "플로깅 목록 조회")
+    @GetMapping("/plogging")
+    public ResponseEntity<? extends BaseResponseDTO> getPloggingList(@ApiParam(value = "페이지 번호") @RequestParam(defaultValue = "1") int page) {
 
         PloggingListWrapperResponseDTO ploggingListWrapperResponseDTO = null;
 
         try {
-        	ploggingListWrapperResponseDTO = ploggingService.getPloggingList(page);
+        	ploggingListWrapperResponseDTO = userInfoService.getPloggingList(page);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(404).body(BaseResponseDTO.of(e.getMessage(), 404));
         } catch (Exception e) {
@@ -42,4 +49,22 @@ public class UserInfoController {
         return ResponseEntity.ok(PloggingListWrapperResponseDTO.of("글 목록 조회에 성공했습니다.", 200, ploggingListWrapperResponseDTO));
 
     }
+    
+    @ApiOperation(value = "방문한 산 목록 조회")
+    @GetMapping("/mountain")
+    public ResponseEntity<? extends BaseResponseDTO> getMountainList(@ApiParam(value = "페이지 번호") @RequestParam(defaultValue = "1") int page) {
+
+    	 Optional<List<String>> mountainList = null;
+
+        try {
+        	mountainList = userInfoService.getMountainList(page);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(404).body(BaseResponseDTO.of(e.getMessage(), 404));
+        } catch (Exception e) {
+            return ResponseEntity.status(409).body(BaseResponseDTO.of("산 목록 조회에 실패했습니다.", 409));
+        }
+        return ResponseEntity.status(200).body(MountainNameListResponseDTO.of("산 목록 조회에 성공했습니다.", 200, MountainNameListResponseDTO.builder().list(mountainList.get()).build()));
+
+    }
+    
 }
