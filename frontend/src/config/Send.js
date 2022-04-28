07497fc7a2c24b4f9ec6114c2343bacef5b773b5
sangjin <toramx5@gmail.com>
2022-04-28 09:59:43 +0900

    fix: send headers error

--- frontend/src/config/Send.js
+++ frontend/src/config/Send.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-let Token = localStorage.getItem("idToken").replace(`"`, " ").replace(`"`, " ");
-
 const instance = axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
-    Authorization: "Bearer" + Token,
+    Authorization: localStorage.getItem("idToken") ? "Bearer" + localStorage.getItem("idToken").replace(`"`, " ").replace(`"`, " ") : null,
   },
 });
 
