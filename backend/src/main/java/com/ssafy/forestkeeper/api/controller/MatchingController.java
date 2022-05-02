f30c7b2d43f512f8103e26d70658fece55f5e60c
GongYunHwan <rhddbsghks@ajou.ac.kr>
2022-05-02 23:04:49 +0900

    feat: 매칭 정원 꽉참 처리

--- backend/src/main/java/com/ssafy/forestkeeper/api/controller/MatchingController.java
+++ backend/src/main/java/com/ssafy/forestkeeper/api/controller/MatchingController.java
@@ -14,10 +14,10 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(value = "Matching API", tags = {"Mathcing"})
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/match")
 public class MatchingController {
 
-    private final MatchingService mathcingService;
+    private final MatchingService matchingService;
     private final MatchingUserService matchingUserService;
 
     @ApiOperation(value = "매칭 글 등록")
@@ -39,7 +39,7 @@ public class MatchingController {
     })
     @PostMapping
     public ResponseEntity<? extends BaseResponseDTO> register(
-        @ApiParam(value = "매칭 글 정보", required = true) @RequestBody @Valid MatchingRegisterPostDTO matchingRegisterPostDTO
+        @ApiParam(value = "매칭 글 등록", required = true) @RequestBody @Valid MatchingRegisterPostDTO matchingRegisterPostDTO
     ) {
 
         if (matchingRegisterPostDTO.getTotal() == 0) {
@@ -47,7 +47,7 @@ public class MatchingController {
         }
 
         try {
-            mathcingService.registerMatching(matchingRegisterPostDTO);
+            matchingService.registerMatching(matchingRegisterPostDTO);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(404).body(BaseResponseDTO.of(e.getMessage(), 404));
         } catch (Exception e) {
@@ -67,7 +67,35 @@ public class MatchingController {
     })
     @PostMapping("/join")
     public ResponseEntity<? extends BaseResponseDTO> joinMatching(
-        @ApiParam(value = "매칭 정보", required = true)@Valid @RequestBody MatchingJoinPostDTO matchingJoinPostDTO
+        @ApiParam(value = "매칭 정보", required = true) @Valid @RequestBody MatchingJoinPostDTO matchingJoinPostDTO
+    ) {
+        if (matchingService.isFull(matchingJoinPostDTO.getMatchingId())) {
+            return ResponseEntity.status(409).body(BaseResponseDTO.of("이미 가득 찬 매칭입니다.", 409));
+        }
+
+        try {
+            matchingUserService.joinMatching(matchingJoinPostDTO.getMatchingId());
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(404).body(BaseResponseDTO.of(e.getMessage(), 404));
+        } catch (Exception e) {
+            System.err.println(e);
+            return ResponseEntity.status(409).body(BaseResponseDTO.of("매칭에 실패했습니다.", 409));
+        }
+
+        return ResponseEntity.status(201).body(BaseResponseDTO.of("매칭 합류에 성공했습니다.", 200));
+
+    }
+
+    @ApiOperation(value = "매칭 마감")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "매칭 마감에 성공했습니다."),
+        @ApiResponse(code = 400, message = "입력된 정보가 유효하지 않습니다."),
+        @ApiResponse(code = 404, message = "필요한 정보를 찾을 수 없습니다."),
+        @ApiResponse(code = 409, message = "매칭 마감에 실패했습니다."),
+    })
+    @PatchMapping("/close")
+    public ResponseEntity<? extends BaseResponseDTO> closeMatching(
+        @ApiParam(value = "매칭 정보", required = true) @Valid @RequestBody MatchingJoinPostDTO matchingJoinPostDTO
     ) {
         try {
             matchingUserService.joinMatching(matchingJoinPostDTO.getMatchingId());
