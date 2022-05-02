a1c1f4feaad224ff0cb2a16cd858501d79f33287
GongYunHwan <rhddbsghks@ajou.ac.kr>
2022-05-03 00:17:52 +0900

    feat: 중복, 풀방 처리

--- backend/src/main/java/com/ssafy/forestkeeper/application/service/mathcing/MatchingUserService.java
+++ backend/src/main/java/com/ssafy/forestkeeper/application/service/mathcing/MatchingUserService.java
@@ -4,4 +4,6 @@ import com.ssafy.forestkeeper.domain.dao.plogging.Matching;
 
 public interface MatchingUserService {
     void joinMatching(String matchingId);
+
+    boolean isJoin(String matchingId);
 }
