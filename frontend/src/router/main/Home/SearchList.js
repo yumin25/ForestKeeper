dc64cf22f10d17a8a83ac4a51d375cf6e3ea8ea4
김수민 <jamie1541@naver.com>
2022-05-02 10:40:10 +0900

    test

--- frontend/src/router/main/Home/SearchList.js
+++ frontend/src/router/main/Home/SearchList.js
@@ -41,48 +41,7 @@ function SearchList({
   let useParam = useParams(); //여기 안에 keyword, category 존재함
   const url = "https://k6a306.p.ssafy.io/api";
   const [searchList, setSearchList] = useState();
-  // const [searchList, setSearchList] = useState([
-  //   {
-  //     mountainCode: "1",
-  //     name: "관악산1",
-  //     address: "서울특별시 상도로 47갈 89 아사달빌딩",
-  //   },
-  //   {
-  //     mountainCode: "2",
-  //     name: "관악산2",
-  //     address: "서울특별시 상도로 47갈 89 아사달빌딩",
-  //   },
-  //   {
-  //     mountainCode: "3",
-  //     name: "관악산3",
-  //     address: "서울특별시 상도로 47갈 89 아사달빌딩",
-  //   },
-  //   {
-  //     mountainCode: "4",
-  //     name: "관악산4",
-  //     address: "서울특별시 상도로 47갈 89 아사달빌딩",
-  //   },
-  //   {
-  //     mountainCode: "4",
-  //     name: "관악산4",
-  //     address: "서울특별시 상도로 47갈 89 아사달빌딩",
-  //   },
-  //   {
-  //     mountainCode: "4",
-  //     name: "관악산4",
-  //     address: "서울특별시 상도로 47갈 89 아사달빌딩",
-  //   },
-  //   {
-  //     mountainCode: "4",
-  //     name: "관악산4",
-  //     address: "서울특별시 상도로 47갈 89 아사달빌딩",
-  //   },
-  //   {
-  //     mountainCode: "4",
-  //     name: "관악산4",
-  //     address: "서울특별시 상도로 47갈 89 아사달빌딩",
-  //   },
-  // ]);
+
   useEffect(() => {
     getResult();
   }, []);
