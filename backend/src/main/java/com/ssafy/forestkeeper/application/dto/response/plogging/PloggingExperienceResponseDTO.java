08e30d533c32f9c1966f56e7cb7280d8d78fdd43
김유민 <elin0225@naver.com>
2022-05-02 11:23:12 +0900

    feat :  플로깅 경험치 부여 API 수정

---
+++ backend/src/main/java/com/ssafy/forestkeeper/application/dto/response/plogging/PloggingExperienceResponseDTO.java
@@ -0,0 +1,23 @@
+package com.ssafy.forestkeeper.application.dto.response.plogging;
+
+import com.ssafy.forestkeeper.application.dto.response.BaseResponseDTO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ApiModel("PloggingExperienceResponseDTO")
+@Getter
+@ToString
+@Builder
+public class PloggingExperienceResponseDTO extends BaseResponseDTO {
+    @ApiModelProperty(name = "경험치")
+    private int exp;
+
+    public static PloggingExperienceResponseDTO of(String message, Integer statusCode, PloggingExperienceResponseDTO ploggingExperienceResponseDTO){
+        ploggingExperienceResponseDTO.setMessage(message);
+        ploggingExperienceResponseDTO.setStatusCode(statusCode);
+        return ploggingExperienceResponseDTO;
+    }
+}
