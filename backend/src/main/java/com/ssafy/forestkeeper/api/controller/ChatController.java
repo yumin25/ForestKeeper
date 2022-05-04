ef6432c399a14a4be38e0340e775875be1c534a0
kibum414 <kibum414@naver.com>
2022-05-04 22:45:51 +0900

    feat: chat message api

---
+++ backend/src/main/java/com/ssafy/forestkeeper/api/controller/ChatController.java
@@ -0,0 +1,35 @@
+package com.ssafy.forestkeeper.api.controller;
+
+import com.ssafy.forestkeeper.application.dto.request.chat.ChatMessageRequestDTO;
+import com.ssafy.forestkeeper.application.service.chat.ChatMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+
+//    private final SimpMessagingTemplate template;
+//
+//    private final ChatMessageService chatMessageService;
+//
+//    @MessageMapping("/chat/enter")
+//    public void enter(ChatMessageRequestDTO chatMessageRequestDTO) {
+//
+//        template.convertAndSend("/topic/room/" + chatMessageRequestDTO.getRoomId(), chatMessageService.enterRoom(chatMessageRequestDTO));
+//
+//    }
+//
+//    @MessageMapping("/chat/room")
+//    public void send(ChatMessageRequestDTO chatMessageRequestDTO) {
+//
+//        template.convertAndSend("/topic/room/" + chatMessageRequestDTO.getRoomId(), chatMessageService.sendMessage(chatMessageRequestDTO));
+//
+//    }
+
+}
+
