9f2d369d486349e394763e9a000fbee57afc465b
kibum414 <kibum414@naver.com>
2022-05-06 23:49:03 +0900

    chore: stomp config

--- backend/src/main/java/com/ssafy/forestkeeper/config/StompWebSocketConfig.java
+++ backend/src/main/java/com/ssafy/forestkeeper/config/StompWebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.ssafy.forestkeeper.config;
 
+import com.ssafy.forestkeeper.util.interceptor.StompHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,13 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("ws-fk")
-                .setAllowedOrigins("http://localhost:8080")
+        registry.addEndpoint("/ws-fk")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
 
     }
@@ -27,4 +33,8 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
 }
