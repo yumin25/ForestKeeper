19b8e57c9d28f8c321bc23affbd32a9d4ba4288a
sangjin <toramx5@gmail.com>
2022-05-10 13:09:39 +0900

    feat: compute plogging distance

--- frontend/src/router/main/plogging/Plogging.js
+++ frontend/src/router/main/plogging/Plogging.js
@@ -20,12 +20,36 @@ function Plogging() {
     }
   };
 
+  // distance
+  const computeDistance = (startLatCord, startLonCord, endLatCord, endLonCord) => {
+    const degreesToRadians = (degrees) => {
+      const radians = (degrees * Math.PI) / 180;
+      return radians;
+    };
+    let startLat = degreesToRadians(startLatCord);
+    let startLon = degreesToRadians(startLonCord);
+    let endLat = degreesToRadians(endLatCord);
+    let endLon = degreesToRadians(endLonCord);
+    const Radius = 6371;
+    let distance = Math.acos(Math.sin(startLat) * Math.sin(endLat) + Math.cos(startLat) * Math.cos(endLat) * Math.cos(startLon - endLon)) * Radius;
+    return distance;
+  };
+  const [allDistance, setAllDistance] = useState([]);
+
   // gps
   const [trackingPath, setTrackingPath] = useState([]);
   const handleTrackingPath = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         setTrackingPath((currentArray) => [...currentArray, new window.naver.maps.LatLng(position.coords.latitude, position.coords.longitude)]);
+        let endLatCord = position.coords.latitude;
+        let endLonCord = position.coords.longitude;
+        let startLatCord = endLatCord;
+        let startLonCord = endLonCord;
+        // console.log(startLatCord, startLonCord, endLatCord, endLonCord);
+        let dist = computeDistance(startLatCord, startLonCord, endLatCord, endLonCord);
+        // console.log(dist);
+        setAllDistance((currentArray) => [...currentArray, dist]);
       });
     }
   };
@@ -37,7 +61,8 @@ function Plogging() {
   };
   const stopTracking = () => {
     clearInterval(tracker);
-    console.log(trackingPath);
+    // console.log(allDistance);
+    // console.log(trackingPath);
   };
 
   // useEffect(() => {
@@ -52,6 +77,7 @@ function Plogging() {
         trackingPath={trackingPath}
         tracking={tracking}
         stopTracking={stopTracking}
+        allDistance={allDistance}
       ></PloggingMap>
     </>
   );
