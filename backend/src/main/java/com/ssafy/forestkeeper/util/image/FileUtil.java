86901943bbee18d4a4654f0ffead581bdc546f0a
염형덕 <gud_ejr@naver.com>
2022-05-11 14:53:56 +0900

    Refactor: 이미지 용량 제한 변경 및 프로필 썸네일 생성

--- backend/src/main/java/com/ssafy/forestkeeper/util/image/FileUtil.java
+++ backend/src/main/java/com/ssafy/forestkeeper/util/image/FileUtil.java
@@ -3,16 +3,15 @@ package com.ssafy.forestkeeper.util.image;
 public class FileUtil {
 
 	private static final String FILE_EXTENSION_SEPARATOR = ".";
-	private static final String CATEGORY_PREFIX = "/";
 	private static final String TIME_SEPARATOR = "_";
 	
 
-	public static String buildFileName(String category, String originalFileName) {
+	public static String buildFileName(String originalFileName) {
 	    int fileExtensionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR);
 	    String fileExtension = originalFileName.substring(fileExtensionIndex);
 	    String fileName = originalFileName.substring(0, fileExtensionIndex);
 	    String now = String.valueOf(System.currentTimeMillis());
 
-	    return category + CATEGORY_PREFIX + fileName + TIME_SEPARATOR + now + fileExtension;
+	    return fileName + TIME_SEPARATOR + now + fileExtension;
 	  }
 }
