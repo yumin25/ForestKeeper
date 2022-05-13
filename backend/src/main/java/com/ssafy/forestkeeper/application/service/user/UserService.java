c66323f78efd81d55ae4597f99f6c83dbbf39136
kibum414 <kibum414@naver.com>
2022-05-14 01:57:54 +0900

    refactor: login api

--- backend/src/main/java/com/ssafy/forestkeeper/application/service/user/UserService.java
+++ backend/src/main/java/com/ssafy/forestkeeper/application/service/user/UserService.java
@@ -4,20 +4,36 @@ package com.ssafy.forestkeeper.application.service.user;
 import com.ssafy.forestkeeper.application.dto.request.user.UserLoginDTO;
 import com.ssafy.forestkeeper.application.dto.request.user.UserSignUpDTO;
 import com.ssafy.forestkeeper.application.dto.response.user.UserInfoDTO;
+import com.ssafy.forestkeeper.application.dto.response.user.UserLoginResponseDTO;
 
 public interface UserService {
+
     Integer signUp(UserSignUpDTO userSignUpDTO);
-    String login(UserLoginDTO userLoginDTO);
+
+    UserLoginResponseDTO login(UserLoginDTO userLoginDTO);
+
     String getUserEmail(String token);
+
     UserInfoDTO getUserDetail();
+
     Integer modifyNickname(String nickname);
+
     Integer modifyPassword(String past_password, String new_password);
-    boolean withdraw();
+
+    void withdraw();
+
     boolean checkNickname(String nickname);
+
     boolean checkEmail(String email);
+
     boolean isValidPassword(String password);
+
     boolean isValidName(String name);
+
     boolean isValidNickname(String nickname);
-    public void updateUserImgPath(String originalName, String savedName);
-    public void registerUserImgPath(String originalFileName, String savedFileName, String email);
+
+    void updateUserImgPath(String originalName, String savedName);
+
+    void registerUserImgPath(String originalFileName, String savedFileName, String email);
+
 }
