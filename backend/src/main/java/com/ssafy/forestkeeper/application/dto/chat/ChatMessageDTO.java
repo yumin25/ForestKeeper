02ca1179fb000ed5a5c53749771d126be8541705
kibum414 <kibum414@naver.com>
2022-05-14 01:06:52 +0900

    feat: add json deserialize and serialize annotation

--- backend/src/main/java/com/ssafy/forestkeeper/application/dto/chat/ChatMessageDTO.java
+++ backend/src/main/java/com/ssafy/forestkeeper/application/dto/chat/ChatMessageDTO.java
@@ -1,5 +1,9 @@
 package com.ssafy.forestkeeper.application.dto.chat;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.ssafy.forestkeeper.domain.enums.MessageCode;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -34,6 +38,8 @@ public class ChatMessageDTO {
     private String content;
 
     @ApiModelProperty(name = "메시지 보낸 시간")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime sendTime;
 
     @ApiModelProperty(name = "메시지 삭제 여부")
