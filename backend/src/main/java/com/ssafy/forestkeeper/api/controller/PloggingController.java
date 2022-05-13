445dc5d0d6e8d283b102e2b1bec1da95ebae035e
kibum414 <kibum414@naver.com>
2022-05-13 19:01:09 +0900

    refactor: extract interface of awss3serviceimpl

--- backend/src/main/java/com/ssafy/forestkeeper/api/controller/PloggingController.java
+++ backend/src/main/java/com/ssafy/forestkeeper/api/controller/PloggingController.java
@@ -1,42 +1,26 @@
 package com.ssafy.forestkeeper.api.controller;
 
 
-import java.util.List;
-import java.util.Optional;
-
-import javax.validation.constraints.NotBlank;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.ssafy.forestkeeper.application.dto.request.plogging.ExpRegisterDTO;
 import com.ssafy.forestkeeper.application.dto.request.plogging.PloggingRegisterDTO;
 import com.ssafy.forestkeeper.application.dto.response.BaseResponseDTO;
-import com.ssafy.forestkeeper.application.dto.response.plogging.MountainPloggingInfoResponseDTO;
-import com.ssafy.forestkeeper.application.dto.response.plogging.PloggingDetailResponseDTO;
-import com.ssafy.forestkeeper.application.dto.response.plogging.PloggingExperienceResponseDTO;
-import com.ssafy.forestkeeper.application.dto.response.plogging.PloggingRegisterResponseDTO;
-import com.ssafy.forestkeeper.application.dto.response.plogging.TrashCanListWrapperResponseDTO;
+import com.ssafy.forestkeeper.application.dto.response.plogging.*;
 import com.ssafy.forestkeeper.application.service.plogging.PloggingAiService;
 import com.ssafy.forestkeeper.application.service.plogging.PloggingService;
-import com.ssafy.forestkeeper.application.service.s3.S3Service;
+import com.ssafy.forestkeeper.application.service.s3.AwsS3Service;
 import com.ssafy.forestkeeper.domain.dao.mountain.TrashCan;
 import com.ssafy.forestkeeper.domain.dao.plogging.Plogging;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+import java.util.Optional;
 
 @Api(value = "Plogging API", tags = {"Plogging"})
 @CrossOrigin("*")
@@ -46,22 +30,22 @@ import lombok.RequiredArgsConstructor;
 public class PloggingController {
 
     private final PloggingService ploggingService;
-    
-    private final S3Service s3Service;
+
+    private final AwsS3Service awsS3Service;
 
     private final PloggingAiService ploggingAiService;
 
     @ApiOperation(value = "플로깅 등록")
     @PostMapping
     public ResponseEntity<?> registerPlogging(@RequestPart(value = "dto", required = true) PloggingRegisterDTO ploggingRegisterDTO,
-    		@RequestPart(value = "image", required = false) MultipartFile multipartFile) {
-    	PloggingRegisterResponseDTO ploggingRegisterResponseDTO;
+                                              @RequestPart(value = "image", required = false) MultipartFile multipartFile) {
+        PloggingRegisterResponseDTO ploggingRegisterResponseDTO;
         try {
-        	Plogging plogging = ploggingService.register(ploggingRegisterDTO);
-        	ploggingRegisterResponseDTO = PloggingRegisterResponseDTO.builder().ploggingId(plogging.getId()).build();
-   	
+            Plogging plogging = ploggingService.register(ploggingRegisterDTO);
+            ploggingRegisterResponseDTO = PloggingRegisterResponseDTO.builder().ploggingId(plogging.getId()).build();
+
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(409).body(BaseResponseDTO.of(e.getMessage(), 409));
+            return ResponseEntity.status(404).body(BaseResponseDTO.of(e.getMessage(), 404));
         } catch (Exception e) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of(e.getMessage(), 409));
         }
@@ -71,17 +55,17 @@ public class PloggingController {
     @ApiOperation(value = "플로깅 상세 조회")
     @GetMapping("/{ploggingId}")
     public ResponseEntity<?> getPloggingDetail(
-        @ApiParam(value = "플로깅 ID", required = true) @PathVariable @NotBlank String ploggingId) {
+            @ApiParam(value = "플로깅 ID", required = true) @PathVariable @NotBlank String ploggingId) {
         PloggingDetailResponseDTO ploggingDetailResponseDTO = null;
         try {
             ploggingDetailResponseDTO = ploggingService.get(ploggingId);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(409).body(BaseResponseDTO.of(e.getMessage(), 409));
+            return ResponseEntity.status(404).body(BaseResponseDTO.of(e.getMessage(), 404));
         } catch (Exception e) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of(e.getMessage(), 409));
         }
         return ResponseEntity.ok(
-            PloggingDetailResponseDTO.of("플로깅 조회에 성공했습니다.", 200, ploggingDetailResponseDTO));
+                PloggingDetailResponseDTO.of("플로깅 조회에 성공했습니다.", 200, ploggingDetailResponseDTO));
     }
 
     @ApiOperation(value = "경험치 부여")
@@ -96,13 +80,13 @@ public class PloggingController {
         }
         return ResponseEntity.status(201).body(BaseResponseDTO.of("경험치 부여에 성공했습니다.", 201));
     }
-    
+
     @ApiOperation(value = "산별 플로깅관련 정보 조회")
     @GetMapping
     public ResponseEntity<?> getMountainPlogging(@RequestParam String mountainCode) {
-    	MountainPloggingInfoResponseDTO ploggingCumulativeResponseDTO;
+        MountainPloggingInfoResponseDTO ploggingCumulativeResponseDTO;
         try {
-        	ploggingCumulativeResponseDTO = ploggingService.getMountainPlogging(mountainCode);
+            ploggingCumulativeResponseDTO = ploggingService.getMountainPlogging(mountainCode);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of(e.getMessage(), 409));
         } catch (Exception e) {
@@ -120,10 +104,10 @@ public class PloggingController {
             List<TrashCan> list = ploggingService.getTrashCanList();
 
             TrashCanListWrapperResponseDTO trashCanListWrapperResponseDTO = TrashCanListWrapperResponseDTO.builder()
-                .list(list).build();
+                    .list(list).build();
 
             return ResponseEntity.ok(TrashCanListWrapperResponseDTO.of("전체 쓰레기통 조회에 성공했습니다.", 200,
-                trashCanListWrapperResponseDTO));
+                    trashCanListWrapperResponseDTO));
 
         } catch (Exception e) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of(e.getMessage(), 409));
@@ -138,16 +122,16 @@ public class PloggingController {
 
             Optional<List<TrashCan>> list = ploggingService.getTrashCanList(region);
 
-            if (!list.isPresent() || list.get().size() == 0) {
+            if (list.isEmpty() || list.get().size() == 0) {
                 return ResponseEntity.status(404)
-                    .body(BaseResponseDTO.of("쓰레기통 데이터가 존재하지 않습니다.", 404));
+                        .body(BaseResponseDTO.of("쓰레기통 데이터가 존재하지 않습니다.", 404));
             }
 
             TrashCanListWrapperResponseDTO trashCanListWrapperResponseDTO = TrashCanListWrapperResponseDTO.builder()
-                .list(list.get()).build();
+                    .list(list.get()).build();
 
             return ResponseEntity.ok(TrashCanListWrapperResponseDTO.of("지역구 쓰레기통 조회에 성공했습니다.", 200,
-                trashCanListWrapperResponseDTO));
+                    trashCanListWrapperResponseDTO));
 
         } catch (Exception e) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of(e.getMessage(), 409));
@@ -160,8 +144,8 @@ public class PloggingController {
         PloggingExperienceResponseDTO ploggingExperienceResponseDTO;
         try {
             ploggingExperienceResponseDTO = ploggingAiService.detectLabels(image, ploggingId);
-    		String savedFileName = s3Service.uploadFileToS3("plogging", image);
-    		ploggingService.registerPloggingImg(image.getOriginalFilename(), savedFileName, ploggingId);
+            String savedFileName = awsS3Service.uploadFileToS3("plogging", image);
+            ploggingService.registerPloggingImg(image.getOriginalFilename(), savedFileName, ploggingId);
         } catch (Exception e) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of("경험치 부여에 실패했습니다.", 409));
         }
--- backend/src/main/java/com/ssafy/forestkeeper/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/forestkeeper/api/controller/UserController.java
@@ -1,32 +1,21 @@
 package com.ssafy.forestkeeper.api.controller;
 
-import java.util.Map;
-
-import com.ssafy.forestkeeper.application.service.s3.S3Service;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.ssafy.forestkeeper.application.dto.request.user.UserLoginDTO;
 import com.ssafy.forestkeeper.application.dto.request.user.UserSignUpDTO;
 import com.ssafy.forestkeeper.application.dto.response.BaseResponseDTO;
 import com.ssafy.forestkeeper.application.dto.response.user.UserInfoDTO;
 import com.ssafy.forestkeeper.application.dto.response.user.UserLoginResponseDTO;
+import com.ssafy.forestkeeper.application.service.s3.AwsS3Service;
 import com.ssafy.forestkeeper.application.service.user.UserService;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @Api(value = "User API", tags = {"User"})
 @CrossOrigin("*")
@@ -37,24 +26,28 @@ public class UserController {
 
     private final UserService userService;
 
-    private final S3Service s3Service;
+    private final AwsS3Service awsS3Service;
 
     @ApiOperation(value = "회원가입")
     @PostMapping
     public ResponseEntity<?> register(@RequestPart(value = "dto", required = true) UserSignUpDTO userSignUpDTO,
-    		@RequestPart(value = "image", required = false) MultipartFile multipartFile) {
+                                      @RequestPart(value = "image", required = false) MultipartFile multipartFile) {
         try {
             Integer result = userService.signUp(userSignUpDTO);
 
-            if(result == 4091) return ResponseEntity.status(409).body(BaseResponseDTO.of("이름 형식이 잘못되었습니다.", 409));
-            else if (result == 4092) return ResponseEntity.status(409).body(BaseResponseDTO.of("닉네임 형식이 잘못되었습니다.", 409));
-            else if (result == 4093) return ResponseEntity.status(409).body(BaseResponseDTO.of("비밀번호 형식이 잘못되었습니다.", 409));
-            else if (result == 4094) return ResponseEntity.status(409).body(BaseResponseDTO.of("해당 이메일로 가입된 계정이 이미 존재합니다.", 409));
-            else if (result == 4095) return ResponseEntity.status(409).body(BaseResponseDTO.of("해당 닉네임으로 가입된 계정이 이미 존재합니다.", 409));
+            if (result == 4091) return ResponseEntity.status(409).body(BaseResponseDTO.of("이름 형식이 잘못되었습니다.", 409));
+            else if (result == 4092)
+                return ResponseEntity.status(409).body(BaseResponseDTO.of("닉네임 형식이 잘못되었습니다.", 409));
+            else if (result == 4093)
+                return ResponseEntity.status(409).body(BaseResponseDTO.of("비밀번호 형식이 잘못되었습니다.", 409));
+            else if (result == 4094)
+                return ResponseEntity.status(409).body(BaseResponseDTO.of("해당 이메일로 가입된 계정이 이미 존재합니다.", 409));
+            else if (result == 4095)
+                return ResponseEntity.status(409).body(BaseResponseDTO.of("해당 닉네임으로 가입된 계정이 이미 존재합니다.", 409));
 
-            if(!multipartFile.isEmpty()) {
-            	String savedFileName = s3Service.uploadFileToS3("user", multipartFile);
-            	userService.registerUserImgPath(multipartFile.getOriginalFilename(), savedFileName, userSignUpDTO.getEmail());
+            if (!multipartFile.isEmpty()) {
+                String savedFileName = awsS3Service.uploadFileToS3("user", multipartFile);
+                userService.registerUserImgPath(multipartFile.getOriginalFilename(), savedFileName, userSignUpDTO.getEmail());
             }
         } catch (Exception e) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of("회원가입에 실패했습니다.", 409));
@@ -64,14 +57,15 @@ public class UserController {
 
     @ApiOperation(value = "로그인")
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserLoginDTO userLoginDTO){
+    public ResponseEntity<?> login(@RequestBody UserLoginDTO userLoginDTO) {
         String result;
-        try{
+        try {
             result = userService.login(userLoginDTO);
         } catch (Exception exception) {
             return ResponseEntity.status(500).body(BaseResponseDTO.of("로그인에 실패하였습니다.", 500));
         }
-        if (result.equals("401")) return ResponseEntity.status(401).body(BaseResponseDTO.of("아이디 또는 비밀번호를 잘못 입력하였습니다.", 401));
+        if (result.equals("401"))
+            return ResponseEntity.status(401).body(BaseResponseDTO.of("아이디 또는 비밀번호를 잘못 입력하였습니다.", 401));
         return ResponseEntity.status(200).body(UserLoginResponseDTO.of(result, "로그인 하였습니다.", 200));
     }
 
@@ -79,11 +73,11 @@ public class UserController {
     @GetMapping("/userinfo")
     public ResponseEntity<?> getUserInfo() {
         UserInfoDTO userInfoDTO;
-        try{
+        try {
             userInfoDTO = userService.getUserDetail();
-        } catch (IllegalStateException e){
+        } catch (IllegalStateException e) {
             return ResponseEntity.status(404).body(BaseResponseDTO.of(e.getMessage(), 404));
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of("사용자 정보 조회에 실패하였습니다.", 409));
         }
         return ResponseEntity.status(200).body(UserInfoDTO.of("사용자 정보 조회에 성공하였습니다.", 200, userInfoDTO));
@@ -92,7 +86,7 @@ public class UserController {
     @ApiOperation(value = "닉네임 중복 확인")
     @GetMapping("/check/nickname")
     public ResponseEntity<?> checkNickname(@RequestParam("nickname") String nickname) {
-        if(userService.checkNickname(nickname)){
+        if (userService.checkNickname(nickname)) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of("사용 중인 닉네임입니다.", 409));
         }
         return ResponseEntity.status(200).body(BaseResponseDTO.of("사용 가능한 닉네임입니다.", 200));
@@ -101,7 +95,7 @@ public class UserController {
     @ApiOperation(value = "이메일 중복 확인")
     @GetMapping("/check/email")
     public ResponseEntity<?> checkEmail(@RequestParam("email") String email) {
-        if(userService.checkEmail(email)){
+        if (userService.checkEmail(email)) {
             return ResponseEntity.status(409).body(BaseResponseDTO.of("사용 중인 이메일입니다.", 409));
         }
         return ResponseEntity.status(200).body(BaseResponseDTO.of("사용 가능한 이메일입니다.", 200));
@@ -112,37 +106,37 @@ public class UserController {
     public ResponseEntity<?> modifyNickname(@RequestParam("nickname") String nickname) {
         Integer result = userService.modifyNickname(nickname);
 
-        if(result == 201) return ResponseEntity.status(201).body(BaseResponseDTO.of("닉네임을 변경하였습니다.", 201));
-        else if(result == 4091) return ResponseEntity.status(409).body(BaseResponseDTO.of("사용 중인 닉네임입니다.", 409));
-        else if(result == 4092) return ResponseEntity.status(409).body(BaseResponseDTO.of("닉네임 형식이 잘못되었습니다.", 409));
+        if (result == 201) return ResponseEntity.status(201).body(BaseResponseDTO.of("닉네임을 변경하였습니다.", 201));
+        else if (result == 4091) return ResponseEntity.status(409).body(BaseResponseDTO.of("사용 중인 닉네임입니다.", 409));
+        else if (result == 4092) return ResponseEntity.status(409).body(BaseResponseDTO.of("닉네임 형식이 잘못되었습니다.", 409));
         else return ResponseEntity.status(500).body(BaseResponseDTO.of("닉네임 변경에 실패하였습니다.", 500));
     }
 
     @ApiOperation(value = "비밀번호 변경")
     @PatchMapping("/modify/password")
-    public ResponseEntity<?> modifyPassword(@RequestBody Map<String,String> param) {
+    public ResponseEntity<?> modifyPassword(@RequestBody Map<String, String> param) {
         String past_password = param.get("past_password");
         String new_password = param.get("new_password");
         Integer result = userService.modifyPassword(past_password, new_password);
 
-        if(result == 201) return ResponseEntity.status(201).body(BaseResponseDTO.of("비밀번호를 변경하였습니다.", 201));
-        if(result == 4091) return ResponseEntity.status(201).body(BaseResponseDTO.of("입력한 비밀번호가 일치하지 않습니다.", 409));
-        if(result == 4092) return ResponseEntity.status(201).body(BaseResponseDTO.of("새로운 비밀번호의 형식이 올바르지 않습니다.", 409));
+        if (result == 201) return ResponseEntity.status(201).body(BaseResponseDTO.of("비밀번호를 변경하였습니다.", 201));
+        if (result == 4091) return ResponseEntity.status(201).body(BaseResponseDTO.of("입력한 비밀번호가 일치하지 않습니다.", 409));
+        if (result == 4092) return ResponseEntity.status(201).body(BaseResponseDTO.of("새로운 비밀번호의 형식이 올바르지 않습니다.", 409));
         return ResponseEntity.status(500).body(BaseResponseDTO.of("비밀번호 변경에 실패하였습니다.", 500));
     }
 
     @ApiOperation(value = "탈퇴")
     @GetMapping("/modify/withdraw")
     public ResponseEntity<?> withdraw() {
-        if(userService.withdraw()) return ResponseEntity.status(201).body(BaseResponseDTO.of("탈퇴하였습니다.", 201));
+        if (userService.withdraw()) return ResponseEntity.status(201).body(BaseResponseDTO.of("탈퇴하였습니다.", 201));
         return ResponseEntity.status(500).body(BaseResponseDTO.of("탈퇴에 실패하였습니다.", 500));
     }
 
-	@PutMapping("/modify/profile")
-	@ApiOperation(value = "회원 프로필 사진 수정")
-	public Object updateUserImage(@RequestBody @ApiParam(value = "회원 프로필 사진.", required = true) MultipartFile image) {
-		String savedFileName = s3Service.uploadFileToS3("user", image);
-		userService.updateUserImgPath(image.getOriginalFilename(), savedFileName);
-		return ResponseEntity.status(201).body(BaseResponseDTO.of("이미지 업로드에 성공했습니다.", 201));
-	}
+    @PutMapping("/modify/profile")
+    @ApiOperation(value = "회원 프로필 사진 수정")
+    public Object updateUserImage(@RequestBody @ApiParam(value = "회원 프로필 사진.", required = true) MultipartFile image) {
+        String savedFileName = awsS3Service.uploadFileToS3("user", image);
+        userService.updateUserImgPath(image.getOriginalFilename(), savedFileName);
+        return ResponseEntity.status(201).body(BaseResponseDTO.of("이미지 업로드에 성공했습니다.", 201));
+    }
 }
