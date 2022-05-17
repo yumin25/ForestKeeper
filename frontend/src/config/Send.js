acb95476d8805fb7004c3ea9d456e228f9327ade
kibum414 <kibum414@naver.com>
2022-05-18 05:57:50 +0900

    fix: axios interceptor including authorization header

--- frontend/src/config/Send.js
+++ frontend/src/config/Send.js
@@ -2,16 +2,20 @@ import axios from "axios";
 
 const token = localStorage.getItem("idToken");
 
+console.log(token);
+
 const instance = axios.create({
   baseURL: "https://k6a306.p.ssafy.io/api/",
   headers: {
     "Content-Type": "application/json",
-    Authorization: token ? "Bearer" + token.replace(`"`, " ").replace(`"`, " ") : null,
+    "Access-Control-Allow-Credentials": true,
   },
 });
 
 instance.interceptors.request.use(
   function (config) {
+    config.headers.Authorization = token ? "Bearer" + token.replace(`"`, " ").replace(`"`, " ") : null;
+
     return config;
   },
   function (error) {
