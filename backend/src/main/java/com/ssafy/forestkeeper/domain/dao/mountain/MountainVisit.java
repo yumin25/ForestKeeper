00871dc6c7ac235f725cd6538550d19758c488a2
kibum414 <kibum414@naver.com>
2022-05-18 00:30:22 +0900

    refactor: rearrange code

--- backend/src/main/java/com/ssafy/forestkeeper/domain/dao/mountain/MountainVisit.java
+++ backend/src/main/java/com/ssafy/forestkeeper/domain/dao/mountain/MountainVisit.java
@@ -1,35 +1,27 @@
 package com.ssafy.forestkeeper.domain.dao.mountain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import com.ssafy.forestkeeper.domain.dao.BaseEntity;
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import javax.persistence.*;
+
 @Entity
 @Builder
 @Getter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class MountainVisit extends BaseEntity{
+public class MountainVisit extends BaseEntity {
 
     @Column(name = "visiter_count")
     private long visiterCount;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mountain_code")
     private Mountain mountain;
-    
+
     public void increaseCount() {
-    	this.visiterCount += 1;
+        this.visiterCount += 1;
     }
 
 }
--- backend/src/main/java/com/ssafy/forestkeeper/domain/dao/plogging/MatchingUser.java
+++ backend/src/main/java/com/ssafy/forestkeeper/domain/dao/plogging/MatchingUser.java
@@ -2,16 +2,9 @@ package com.ssafy.forestkeeper.domain.dao.plogging;
 
 import com.ssafy.forestkeeper.domain.dao.BaseEntity;
 import com.ssafy.forestkeeper.domain.dao.user.User;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Entity
 @Builder
@@ -35,9 +28,9 @@ public class MatchingUser extends BaseEntity {
     public void changeDeleteTrue() {
         this.delete = true;
     }
-    
+
     public void changeDeleteFalse() {
-    	this.delete = false;
+        this.delete = false;
     }
 
 }
