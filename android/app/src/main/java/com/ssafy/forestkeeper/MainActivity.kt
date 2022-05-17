361a491c38dfb9eafc6e19259f81a5eda09baee4
김유민 <elin0225@naver.com>
2022-05-17 15:23:06 +0900

    refactor: save image refactor

--- android/app/src/main/java/com/ssafy/forestkeeper/MainActivity.kt
+++ android/app/src/main/java/com/ssafy/forestkeeper/MainActivity.kt
@@ -88,58 +88,63 @@ class MainActivity : AppCompatActivity() {
                 filePathCallback: ValueCallback<Array<Uri>>?,
                 fileChooserParams: FileChooserParams?
             ): Boolean {
-                try{ mWebViewImageUpload = filePathCallback!!
-                    var takePictureIntent : Intent?
-                    takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    if(takePictureIntent.resolveActivity(packageManager) != null){
-                        var photoFile : File?
-                        photoFile = createImageFile()
-                        takePictureIntent.putExtra("PhotoPath",cameraPath)
-                        if(photoFile != null){
-                            cameraPath = "file:${photoFile.absolutePath}"
-                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(photoFile))
-                        }
-                        else takePictureIntent = null
-                    }
-                    val contentSelectionIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                    contentSelectionIntent.type = "image/*"
-                    var intentArray: Array<Intent?>
-                    if(takePictureIntent != null) intentArray = arrayOf(takePictureIntent)
-                    else intentArray = takePictureIntent?.get(0)!!
-                    val chooserIntent = Intent(Intent.ACTION_CHOOSER)
-                    chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
-                    chooserIntent.putExtra(Intent.EXTRA_TITLE,"사용할 앱을 선택해주세요.")
-                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
-                    launcher.launch(chooserIntent)
+                try{
+                    selectImage(filePathCallback)
                 } catch (e : Exception){ }
                 return true
             }
-
-            fun createImageFile(): File? {
-                @SuppressLint("SimpleDateFormat")
-                val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-                val imageFileName = "img_" + timeStamp + "_"
-                val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-                return File.createTempFile(imageFileName, ".jpg", storageDir)
-            }
-
-            val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == RESULT_OK) {
-                    val intent = result.data
-                    if(intent == null){ //바로 사진을 찍어서 올리는 경우
-                        val results = arrayOf(Uri.parse(cameraPath))
-                        mWebViewImageUpload!!.onReceiveValue(results!!)
-                    } else{ //사진 앱을 통해 사진을 가져온 경우
-                        val results = intent!!.data!!
-                        mWebViewImageUpload!!.onReceiveValue(arrayOf(results!!))
-                    }
-                } else{ //취소 한 경우 초기화
-                    mWebViewImageUpload!!.onReceiveValue(null)
-                    mWebViewImageUpload = null
-                }
-            }
         }
         webView.loadUrl("https://k6a306.p.ssafy.io/")
+    }
+
+    fun selectImage(filePathCallback: ValueCallback<Array<Uri>>?){
+        mWebViewImageUpload = filePathCallback!!
+        var takePictureIntent : Intent?
+        takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(takePictureIntent.resolveActivity(packageManager) != null){
+            var photoFile : File?
+            photoFile = createImageFile()
+            takePictureIntent.putExtra("PhotoPath",cameraPath)
+            if(photoFile != null){
+                cameraPath = "file:${photoFile.absolutePath}"
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(photoFile))
+            }
+            else takePictureIntent = null
+        }
+        val contentSelectionIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        contentSelectionIntent.type = "image/*"
+        var intentArray: Array<Intent?>
+        if(takePictureIntent != null) intentArray = arrayOf(takePictureIntent)
+        else intentArray = takePictureIntent?.get(0)!!
+        val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+        chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
+        chooserIntent.putExtra(Intent.EXTRA_TITLE,"사용할 앱을 선택해주세요.")
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
+        launcher.launch(chooserIntent)
+    }
+
+    fun createImageFile(): File? {
+        @SuppressLint("SimpleDateFormat")
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "img_" + timeStamp + "_"
+        val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(imageFileName, ".jpg", storageDir)
+    }
+
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            if(intent == null){ //바로 사진을 찍어서 올리는 경우
+                val results = arrayOf(Uri.parse(cameraPath))
+                mWebViewImageUpload!!.onReceiveValue(results!!)
+            } else{ //사진 앱을 통해 사진을 가져온 경우
+                val results = intent!!.data!!
+                mWebViewImageUpload!!.onReceiveValue(arrayOf(results!!))
+            }
+        } else{ //취소 한 경우 초기화
+            mWebViewImageUpload!!.onReceiveValue(null)
+            mWebViewImageUpload = null
+        }
     }
 
     override fun onBackPressed() {
